a6b25a92f1e2e3a3d350877ef50145f6cf9c19d1
Fin Christensen <christensen.fin@gmail.com>
2018-04-24 18:05:01 +0200

    Add Dockerfile to project

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ Authors:
 	app := cli.NewApp()
 	app.Name = "burrow"
 	app.Usage = "A go build system that uses glide for dependency management."
-	app.Version = "0.0.13"
+	app.Version = "0.1.0"
 	app.Authors = []cli.Author{
 		{
 			Name:  "Fin Christensen",
